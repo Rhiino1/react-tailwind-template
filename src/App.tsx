2f58da6188b97a6594e697c936e7bed74054bce9
Cesar SC <csolanoc@unal.edu.co>
2023-09-23 11:38:12 -0500

    Added gh-pages. Created footer component. Added alias import.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-function App() {
+import Footer from "@/layouts/Footer";
 
+function App() {
   return (
     <>
-    <h1 className="text-5xl font-bold text-red-300">Tailwindcss working</h1>
+      <div className="flex flex-col h-screen justify-between bg-[#d5e1ef]">
+        <h1 className="text-5xl font-bold text-red-300">Tailwindcss working</h1>
+        <Footer></Footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <>
+
+      <footer className="text-[11px] text-center w-full h-auto">
+        Challenge by{" "}
+        <a className="text-[#3E52A3]" href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a className="text-[#3E52A3]" href="#">Cesar SC</a>.
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
